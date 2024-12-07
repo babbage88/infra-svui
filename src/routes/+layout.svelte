f63c6d0b64b3521a28fb855676d6a72b8081d054
Justin Trahan <justin.m.trahan@outlook.com>
2024-12-07 02:23:27 +0000

    Added DarkMode toggle switch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from "mode-watcher";
+	import DarkModeToggle from '$lib/components/ui/lightswitch/DarkModeToggle.svelte';
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+<div class="flex flex-col min-h-screen">
+	<header class="flex justify-end items-center p-4">
+		<DarkModeToggle />
+	</header>
+	<main class="flex-1 p-4">
+		{@render children()}
+	</main>
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -22,23 +22,22 @@
     }
   };
   </script>
-
-<main class="flex items-center justify-center min-h-screen">
-    <div class="w-1/3 mx-auto bg-white p-8 shadow-lg rounded-lg">
+<div class="flex items-center justify-center min-h-screen">
+    <div class="w-1/3 mx-auto p-8 shadow-lg rounded-lg">
       <h1 class="text-2xl font-bold text-center mb-6">Login</h1>
       <form on:submit={handleSubmit}>
         <div class="mb-4">
-          <Label class="block text-gray-700 mb-2" for="username">Username:</Label>
-          <Input class="w-full border border-gray-300 rounded-lg p-2" id="username" type="username" bind:value={username} required />
+          <Label class="block mb-2" for="username">Username:</Label>
+          <Input class="w-full border rounded-lg p-2" id="username" type="username" bind:value={username} required />
         </div>
         <div class="mb-4">
-          <Label class="block text-gray-700 mb-2" for="password">Password:</Label>
-          <Input class="w-full border border-gray-300 rounded-lg p-2" id="password" type="password" bind:value={password} required />
+          <Label class="block mb-2" for="password">Password:</Label>
+          <Input class="w-full borde rounded-lg p-2" id="password" type="password" bind:value={password} required />
         </div>
         {#if error}
-          <p class="text-red-500 text-sm mb-4">{error}</p>
+          <p class="text-sm mb-4">{error}</p>
         {/if}
         <Button type="submit" class="w-full">Login</Button>
       </form>
     </div>
-  </main>
+  </div>
