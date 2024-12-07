a23423e9445e4e8e11eb75605019743ccc69e46b
Justin Trahan <justin.m.trahan@outlook.com>
2024-12-07 04:54:22 +0000

    testing nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import { ModeWatcher } from "mode-watcher";
+	import  ExamplesNav  from '$lib/components/ui/examples-nav/examples-nav.svelte';
 	import DarkModeToggle from '$lib/components/ui/lightswitch/DarkModeToggle.svelte';
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
+<ExamplesNav />
 <div class="flex flex-col min-h-screen">
 	<header class="flex justify-end items-center p-4">
 		<DarkModeToggle />
