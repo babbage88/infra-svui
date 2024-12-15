6afc94d2048cc93aba0e611d0040161886598b50
Justin Trahan <justin.m.trahan@outlook.com>
2024-12-15 18:19:38 +0000

    table sort working

--- src/routes/users/data-table.svelte
+++ src/routes/users/data-table.svelte
@@ -2,9 +2,9 @@
 	import { writable, type Writable } from 'svelte/store'; // Import writable store
 	import { createTable, Render, Subscribe, createRender } from 'svelte-headless-table';
 	import * as Table from '$lib/components/ui/table';
-	import { addPagination } from 'svelte-headless-table/plugins';
+	import { addPagination, addSortBy } from 'svelte-headless-table/plugins';
+	import ArrowUpDown from 'lucide-svelte/icons/arrow-up-down';
 	import DataTableActions from './data-table-actions.svelte';
-	import { onDestroy } from 'svelte';
 	import type { User } from '$lib/services/user';
 	import { UserRolesBadge } from '$lib/components/ui/badge-with-props/index.js';
 	import { Button } from '$lib/components/ui/button';
@@ -19,26 +19,47 @@
 
 	// Create the table based on the users data
 	const table = createTable(users, {
-		page: addPagination()
+		page: addPagination(),
+		sort: addSortBy()
 	});
 	const columns = table.createColumns([
 		table.column({
 			accessor: 'id',
-			header: 'ID'
+			header: 'ID',
+			plugins: {
+				sort: {
+					disable: false
+				}
+			}
 		}),
 		table.column({
 			accessor: 'username',
-			header: 'Username'
+			header: 'Username',
+			plugins: {
+				sort: {
+					disable: false
+				}
+			}
 		}),
 		table.column({
 			accessor: 'email',
-			header: 'Email'
+			header: 'Email',
+			plugins: {
+				sort: {
+					disable: false
+				}
+			}
 		}),
 		table.column({
 			accessor: 'role',
 			header: 'Role',
 			cell: ({ value }) => {
 				return createRender(UserRolesBadge, { value: value });
+			},
+			plugins: {
+				sort: {
+					disable: false
+				}
 			}
 		}),
 		table.column({
@@ -46,6 +67,11 @@
 			header: '',
 			cell: ({ value }) => {
 				return createRender(DataTableActions, { id: value });
+			},
+			plugins: {
+				sort: {
+					disable: true
+				}
 			}
 		})
 	]);
@@ -64,9 +90,31 @@
 					<Subscribe rowAttrs={headerRow.attrs()}>
 						<Table.Row>
 							{#each headerRow.cells as cell (cell.id)}
-								<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()}>
-									<Table.Head {...attrs}>
+								<Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
+                  <Table.Head {...attrs}>
+                  {#if cell.id === "id"}
+                  <Button variant="ghost" on:click={props.sort.toggle}>
+                    <Render of={cell.render()} />
+                    <ArrowUpDown class={"ml-2 h-4 w-4"} />
+                  </Button>
+                  {:else if cell.id === "username"}
+                  <Button variant="ghost" on:click={props.sort.toggle}>
+                    <Render of={cell.render()} />
+                    <ArrowUpDown class={"ml-2 h-4 w-4"} />
+                  </Button>
+                  {:else if cell.id ==="email"}
+                  <Button variant="ghost" on:click={props.sort.toggle}>
+                    <Render of={cell.render()} />
+                    <ArrowUpDown class={"ml-2 h-4 w-4"} />
+                  </Button>
+                  {:else if cell.id === "role"}
+                  <Button variant="ghost" on:click={props.sort.toggle}>
+                    <Render of={cell.render()} />
+                    <ArrowUpDown class={"ml-2 h-4 w-4"} />
+                  </Button>
+                  {:else}
 										<Render of={cell.render()} />
+                  {/if}
 									</Table.Head>
 								</Subscribe>
 							{/each}
